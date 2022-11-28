ac3c1acbd44135e7d648201adf45a78ddd09534c
HeyPalu <j.l.strunz@gmail.com>
2022-11-28 21:35:35 +0100

    refactor globalCommands/add.js

--- globalCommands/add.js
+++ globalCommands/add.js
@@ -13,40 +13,37 @@ module.exports = {
     }),
   async execute(interaction) {
     const optionUsername = interaction.options.getString('username')
+    if (interaction.guild != null) {
+      return await interaction.reply({
+        content: `:x: Please use this command only in DMs`,
+        ephemeral: true,
+      })
+    }
     if (optionUsername != interaction.user.tag) {
-      await interaction.reply({
+      return await interaction.reply({
         content: `:x: Your username doesn't fit the one you provided!\n:exclamation: Provided username: ${optionUsername}\n:exclamation: Your username: ${interaction.user.tag}\n:question: Try /help for more information`,
         ephemeral: false,
       })
-    } else {
-      if (interaction.guild != null) {
-        await interaction.reply({
-          content: `:x: Please use this command only in DMs`,
-          ephemeral: true,
+    }
+    try {
+      let database = JSON.parse(fs.readFileSync('./data.json'))
+      if (database.verifiedUsers.includes(optionUsername)) {
+        return await interaction.reply({
+          content: `:x: ${optionUsername} has already been added to the database!`,
+          ephemeral: false,
         })
-      } else {
-        try {
-          let database = JSON.parse(fs.readFileSync('./data.json'))
-          if (database.verifiedUsers.includes(optionUsername)) {
-            await interaction.reply({
-              content: `:x: ${optionUsername} has already been added to the database!`,
-              ephemeral: false,
-            })
-          } else {
-            database.verifiedUsers.push(optionUsername)
-            fs.writeFileSync('./data.json', JSON.stringify(database))
-            await interaction.reply({
-              content: `:white_check_mark: Successfully added ${optionUsername} to the database!`,
-              ephemeral: false,
-            })
-          }
-        } catch {
-          await interaction.reply({
-            content: `:x: Couldn't add ${optionUsername} to the database!`,
-            ephemeral: false,
-          })
-        }
       }
+      database.verifiedUsers.push(optionUsername)
+      fs.writeFileSync('./data.json', JSON.stringify(database))
+      await interaction.reply({
+        content: `:white_check_mark: Successfully added ${optionUsername} to the database!`,
+        ephemeral: false,
+      })
+    } catch {
+      await interaction.reply({
+        content: `:x: Couldn't add ${optionUsername} to the database!`,
+        ephemeral: false,
+      })
     }
   },
 }
