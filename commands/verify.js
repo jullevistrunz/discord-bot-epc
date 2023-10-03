a68f6c5d1fde2532af1b65e45674877849dc8b8b
HeyPalu <j.l.strunz@gmail.com>
2023-10-03 17:44:23 +0200

    server update

--- commands/verify.js
+++ commands/verify.js
@@ -6,29 +6,20 @@ module.exports = {
     .setName('verify')
     .setDescription('Verify on this Server'),
   async execute(interaction) {
-    //command will only work in guild as it will only be registered in there
-    const database = JSON.parse(fs.readFileSync('./data.json'))
-    if (!database.verifiedUsers.includes(interaction.user.tag)) {
+    try {
+      const role = interaction.guild.roles.cache.find(
+        (r) => r.name == 'Verified'
+      )
+      interaction.member.roles.add(role)
       await interaction.reply({
-        content: `:x: Your username is not in the database\n:question: Try /help for more information`,
+        content: `:white_check_mark: Successfully verified ${interaction.user.tag}!`,
         ephemeral: true,
       })
-    } else {
-      try {
-        const role = interaction.guild.roles.cache.find(
-          (r) => r.name == 'Verified'
-        )
-        interaction.member.roles.add(role)
-        await interaction.reply({
-          content: `:white_check_mark: Successfully verified ${interaction.user.tag}!`,
-          ephemeral: true,
-        })
-      } catch {
-        await interaction.reply({
-          content: `:x: Couldn't verify ${interaction.user.tag}!`,
-          ephemeral: true,
-        })
-      }
+    } catch {
+      await interaction.reply({
+        content: `:x: Couldn't verify ${interaction.user.tag}!`,
+        ephemeral: true,
+      })
     }
   },
 }
